c3208ba786e4e12294b619ac356818e1e8096a63
Emmyy882 <emmaagbaje3@gmail.com>
2023-10-13 12:19:57 +0100

    AirBnB_clone project files update

--- console.py
+++ console.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""This is a command interpreter"""
+"""This is a command interpreter for the AirBnB_clone project"""
 
 
 import cmd
 import os
 import sys
 
-class Interpreter(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     """
         A command interpreter that prints a prompt to the console
         for the AirBnB Project
     """
     
-    # prompt to print if in interactive mode
+    # custom prompt to print if in interactive mode
     prompt = "(hbnb) " if sys.stdin.isatty() else ''
 
-    # A Dictionary that maps class names to their corresponding classes
-    classes = {
-            'User': User,
-            'State': State,
-            'City': City,
-            'Place': Place
-            }
+    def do_quit(self, arg):
+        """Quit command to exit the program\n"""
+        return True
 
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
