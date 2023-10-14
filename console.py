e77cb80a886d2106a35d07fb146ccfe9f911f8a3
Emmyy882 <emmaagbaje3@gmail.com>
2023-10-14 23:09:52 +0100

    AirBnB_clone project files update

--- console.py
+++ console.py
@@ -5,11 +5,12 @@
 import cmd
 import os
 import sys
+#from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
-        A command interpreter that prints a prompt to the console
-        for the AirBnB Project
+    A command interpreter that prints a prompt to the console
+    for the AirBnB Project
     """
     
     # custom prompt to print if in interactive mode
@@ -20,11 +21,35 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, arg):
-        """Quit command to exit the program"""
-        print()
-        return True
+        """Handles the command of "End Of File" to 
+        exit the program"""
+        exit (0)
 
     def emptyline(self):
+        pass
+
+    def do_create(self, args):
+        """
+        Create Command
+        
+        Creates a new instance of a Class.
+        """
+        try:
+            cls = globals()[BaseModel]
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+        except KeyError:
+            print("** class name missing **")
+        except NameError:
+            print("class doesn't exist")
+
+    def do_show(self, args):
+        """
+        Show Command.
+
+        prints the string representation of an instance based on the class name and id
+        """
         pass
 
 if __name__ == '__main__':
