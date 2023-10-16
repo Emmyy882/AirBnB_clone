eec2edb957670cf5a24df8478671304aa1c7ed75
Emmyy882 <emmaagbaje3@gmail.com>
2023-10-16 22:35:38 +0100

    AirBnB_clone project files update

--- models/base_model.py
+++ models/base_model.py
@@ -52,7 +52,8 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the BaseModel instance"""
         cls = self.__class__.__name__
-        return "{[]} {()} {}".format(cls, self.id, self.__dict__)
+        return "{[]} {()} {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def delete(self):
         """
