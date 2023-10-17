d3cb1d064a7abb5289c73b37842ccd32e4ace33b
Emmyy882 <emmaagbaje3@gmail.com>
2023-10-17 09:41:56 +0100

    AirBnB_clone project files update

--- console.py
+++ console.py
@@ -7,7 +7,13 @@ import os
 import sys
 
 from models.base_model import BaseModel
-from models import storage, user, place, state, city, amenity, review
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
